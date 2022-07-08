ebd46675cd176582c61a66e13b97b33be2220d85
Katelind Phillips <scarlettkate94@gmail.com>
2022-07-08 15:13:48 -0400

    prelim

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let watching = new Promise(userWatchingLiveStream)
+{
+    if(userWatchingLiveStream)
+    {
+        resolve("Thumbs up and Subscribe!");
+    }
+    else
+    {
+        PromiseRejectionEvent("User left.");
+    }
+}
+
+watching.then();
+watching.catch();
